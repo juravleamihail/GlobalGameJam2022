d06d922e8793420a254f51bb30354ada4647c4b8
bogheorghiu <bogheorghiu88@gmail.com>
2022-01-23 00:26:00 +0200

    mid-progress commit, need to pull latest

--- Assets/Scripts/Samurai/DrawPath.cs
+++ Assets/Scripts/Samurai/DrawPath.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class DrawPath : MonoBehaviour
 {
-    //TODO implement connection with keys
+    //TODO implement connection with keys and state machine
 
-
+    //subscribe the stuff in this class to delegates in the input handler
 }
