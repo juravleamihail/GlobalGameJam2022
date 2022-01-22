0132f7c33534d64ba3ce3429bf6dba454eecadb2
RomIdea <romidea.dev@gmail.com>
2022-01-22 22:25:24 +0200

    added playermanager

---
+++ Assets/Scripts/NinjaManager.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NinjaManager : MonoBehaviour
+{
+    private static NinjaManager _instance;
+    public static NinjaManager Instance
+    {
+        get
+        {
+            return _instance;
+        }
+    }
+
+    private Dictionary<int,List<Ninja>> ninjaList;
+
+   
+    private void Awake()
+    {
+        ninjaList = new Dictionary<int, List<Ninja>>();
+
+        if (_instance == null)
+        {
+            _instance = this;
+        }
+        else if (_instance != null)
+        {
+            Destroy(this);
+        }
+    }
+
+    /*internal void AddNinja(int playerIndex)
+    {
+       if(ninja == null)
+        {
+            return;
+        }
+
+        ninjaList.Add(ninja);
+    }
+
+
+    public List<Ninja> GetNinjaByPlayer(int playerIndex)
+    {
+        return ninjaList.FindAll(samurai => samurai.NinjaType.PlayerIndex == playerIndex);
+    }*/
+
+    
+}
--- Assets/Scripts/NinjaTypeSO.cs
+++ Assets/Scripts/NinjaTypeSO.cs
@@ -2,8 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(menuName ="SamuraiSO")]
-public class SamuraiSO : ScriptableObject
+
+[CreateAssetMenu(menuName = "Ninja/NinjaTypeSO")]
+public class NinjaTypeSO : ScriptableObject
 {
     public string NameText;
     public int PlayerIndex;
