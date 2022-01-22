a4896577b8c905d60d4ea1e09a2d02bcc40c3d9a
bogheorghiu <bogheorghiu88@gmail.com>
2022-01-22 17:32:38 +0200

    grid system updates written but not yet tested. created temporary GridTest class

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,55 +5,69 @@ using UnityEngine;
 
 public class GameManager : Singleton<GameManager>
 {
-   [Space(10), SerializeField] private GameSettingsSO _gameSettings;
-   [Space(25), SerializeField] private MainMenu _mainMenuUI;
-   [SerializeField] private Hud _hudUI;
+    [Space(10), SerializeField] private GameSettingsSO _gameSettings;
+    [Space(25), SerializeField] private MainMenu _mainMenuUI;
+    [SerializeField] private Hud _hudUI;
    
-   
-   private StateMachine _stateMachine;
-   private GridSystem _grid;
+    private StateMachine _stateMachine;
+    private GridSystem _grid;
 
-   public override void Awake()
-   {
-      base.Awake();
-      _stateMachine = new StateMachine();
-      _grid = new GridSystem(_gameSettings.GridSize, _gameSettings.TileSize);
-   }
+    public override void Awake()
+    {
+        base.Awake();
+        _stateMachine = new StateMachine();
+        _grid = new GridSystem(_gameSettings.GridSize, _gameSettings.TileSize);
+    }
 
-   private void Update()
-   {
-      _stateMachine.UpdateState();
-   }
+    private void Update()
+    {
+        _stateMachine.UpdateState();
+    }
 
-   public void StartGame()
-   {
-      _stateMachine.ChangeState(new TurnState(_gameSettings.Timer, _hudUI.UpdateTimer,null));
-      ToggleUI(UIStates.Gameplay);
-   }
+    public void StartGame()
+    {
+        _stateMachine.ChangeState(new TurnState(_gameSettings.Timer, _hudUI.UpdateTimer,null));
+        ToggleUI(UIStates.Gameplay);
+    }
 
-   public void BackToMainMenu()
-   {
-      ToggleUI(UIStates.MainMenu);
-   }
+    public void BackToMainMenu()
+    {
+        ToggleUI(UIStates.MainMenu);
+    }
 
-   public void ExitGame()
-   {
-      Application.Quit();
-   }
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
 
-   private void ToggleUI(UIStates uiState)
-   {
-      _mainMenuUI.gameObject.SetActive(uiState == UIStates.MainMenu);
-      _hudUI.gameObject.SetActive(uiState == UIStates.Gameplay);
-   }
+    private void ToggleUI(UIStates uiState)
+    {
+        _mainMenuUI.gameObject.SetActive(uiState == UIStates.MainMenu);
+        _hudUI.gameObject.SetActive(uiState == UIStates.Gameplay);
+    }
 
-   private enum UIStates{
-      MainMenu,
-      Gameplay
-   }
+    private enum UIStates{
+        MainMenu,
+        Gameplay
+    }
 
-   private Vector3 ConvertGridCoordsToVector3(uint gridX, uint gridY)
-   {
-      return _grid.ConvertGridCoordsToVector3(gridX, gridY);
-   }
+    public Vector3 ConvertGridCoordsToVector3(uint gridX, uint gridY)
+    {
+        return _grid.ConvertGridCoordsToVector3(gridX, gridY);
+    }
+
+    public Vector2Int ConvertVector3CoordsToGrid(float worldX, float worldZ)
+    {
+        return _grid.ConvertVector3ToGridCoords(worldX, worldZ);
+    }
+
+    public Vector2Int MoveOneTileOnGrid(uint currentGridX, uint currentGridY, GridSystem.Directions direction)
+    {
+        return _grid.MoveOneTileOnGrid(currentGridX, currentGridY, direction);
+    }
+
+    public Vector3 MoveOneTileInWorld(Vector3 currentPosition, GridSystem.Directions direction)
+    {
+        return _grid.MoveOneTileInWorld(currentPosition, direction);
+    }
 }
